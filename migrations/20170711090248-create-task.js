caeb95bbfb766e57f5758d944a4f738ec130fb7c
adrian mui <adrian.mui@gmail.com>
2017-07-11 04:24:30 -0500

    did the migrations

--- migrations/20170711090248-create-task.js
+++ migrations/20170711090248-create-task.js
@@ -17,7 +17,7 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'User',
+          model: 'Users',
           key: 'id'
         },
       },
