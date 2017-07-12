0d88b038c3e73492123ef6445e6483dff56b1a18
adrian mui <adrian.mui@gmail.com>
2017-07-12 02:58:53 -0500

    added passport to existing users

--- src/server/auth/router.js
+++ src/server/auth/router.js
@@ -1,20 +1,45 @@
 const router = require('express').Router();
 const passport = require('./local');
+const auth = require('./auth');
 
 const User = require('./../api/user/model');
 
-router.post('/register', (req, res, next)  => {
-  return User.find({where : {email: req.body.email}})
-  .then((response) => {
-    passport.authenticate('local', (err, user, info) => {
-      if (user) { handleResponse(res, 200, 'success'); }
-    })(req, res, next);
-  })
-  .catch((err) => { handleResponse(res, 500, 'error'); });
+router.post('/register', auth.loginRedirect, (req, res, next)  => {
+  return User.create({email: req.body.email})
+    .then((response) => {
+      passport.authenticate('local', (err, user, info) => {
+        if (user) {
+          req.logIn(user, err => {
+            if (err) handleResponse(res, 500, 'error');
+            handleResponse(res, 200, 'success'); 
+          });
+        }
+      })(req, res, next);
+    })
+    .catch((err) => { handleResponse(res, 500, 'error'); });
+});
+
+router.post('/login', auth.loginRedirect, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    console.log(`adrian snoping ${JSON.stringify(user.get({plain: true}))}`)
+    if (err) { handleResponse(res, 500, 'error'); }
+    if (!user) { handleResponse(res, 404, 'User not found'); }
+    if (user) {
+      req.logIn(user, function (err) {
+        if (err) { handleResponse(res, 500, 'error'); }
+        handleResponse(res, 200, 'success');
+      });
+    }
+  })(req, res, next);
+});
+
+router.get('/logout', auth.loginRequired, (req, res, next) => {
+  req.logout();
+  handleResponse(res, 200, 'success');
 });
 
 function handleResponse(res, code, statusMsg) {
   res.status(code).json({status: statusMsg});
 }
 
-module.exports = router
+module.exports = router;
--- test/integration/auth.test.js
+++ test/integration/auth.test.js
@@ -1,26 +1,43 @@
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
 const server = require('./../../src/server/server');
 const db = require('./../../src/server/config/db');
 
-describe('routes : auth', () => {
-  describe('POST /auth/register', () => {
-    it('should register a new user', (done) => {
-      chai.request(server)
-      .post('/auth/register')
-      .send({
-        email: 'admin@admin.com'
-      })
-      .end((err, res) => {
-        should.not.exist(err);
-        res.redirects.length.should.eql(0);
-        res.status.should.eql(200);
-        res.type.should.eql('application/json');
-        res.body.status.should.eql('success');
-        done();
-      });
+chai.use(chaiHttp);
+
+describe('POST /auth/login', () => {
+  it('should login a user', (done) => {
+    chai.request(server)
+    .post('/auth/login')
+    .send({email: 'test@test.com'})
+    .end((err, res) => {
+      should.not.exist(err);
+      res.redirects.length.should.eql(0);
+      res.status.should.eql(200);
+      res.type.should.eql('application/json');
+      res.body.status.should.eql('success');
+      done();
     });
+  });
+});
+
+describe('GET /auth/logout', () => {
+  it('should logout a user after he has logged in', (done) => {
+    let chow = chai.request(server);
+
+    chow.post('/auth/login')
+      .send({email: 'admin@admin.com'})
+      .then(() => {
+        chow.get('/auth/logout')
+          .end((err, res) => {
+            should.not.exist(err);
+            res.redirects.length.should.eql(0);
+            res.status.should.eql(200);
+            res.type.should.eql('application/json');
+            res.body.status.should.eql('success');
+            done();
+          });
+      })
   });
 });
