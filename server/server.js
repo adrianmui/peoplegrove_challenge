a231eb17988c17128edd3e6de57db5a0bf5682a7
adrian mui <adrian.mui@gmail.com>
2017-07-12 00:19:18 -0500

    added passport module, but before this i need to move my whole file directory down into src

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
-
-const index = require('./../routes/index');
-const users = require('./../routes/users');
-
 const app = express();
+
+const config = require('./config/config');
+
+const apiRouter = require('./api/api.js');
+
+const passport = require('passport');
+const flash    = require('connect-flash');
+const morgan       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const session      = require('express-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -13,12 +20,19 @@ app.set('view engine', 'ejs');
 // apply middleware
 require('./middleware/middleware')(app);
 
+//passport
+app.use(session({ secret: config.secret })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // serving public folder
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // routes
-app.use('/index', index);
-app.use('/users', users);
+app.use('/api', apiRouter);
+// interface routes
+require('./../routes/index')(app, passport);
 
 // home interface page
 app.get('/', function(req, res) {
